7d800a48f48de968db80f7c4c77b1413b97d560f
raashed <mohammedraashed10@gmail.com>
2026-04-23 13:46:36 +0530

    feat(gorm): added gorm support for the /api

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,2 +1,13 @@
 // Package store deals with all the db-level stuff in here
 package store
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primarykey" json:"id"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `gorm:"not null" json:"name"`
+	Password  string    `gorm:"not null" json:"password"`
+	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
+}
